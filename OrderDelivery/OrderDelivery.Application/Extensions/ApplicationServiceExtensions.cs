2bc2fb0fa0b0a65734916929c00205a0669ce45a
WaelAlfanan <waelbahaa01@hotmail.com>
2025-06-26 15:12:51 +0100

    Init

---
+++ OrderDelivery/OrderDelivery.Application/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderDelivery.Application.Extensions
+{
+    public class ApplicationServiceExtensions
+    {
+    }
+}
