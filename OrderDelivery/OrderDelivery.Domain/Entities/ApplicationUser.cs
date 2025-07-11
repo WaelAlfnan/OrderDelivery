fade363c663145ee089b0ddbd3c6628a51619338
WaelAlfnan <waelbahaa01@hotmail.com>
2025-07-11 17:33:35 +0100

    Created OrderDeliveryDbContext

--- OrderDelivery/OrderDelivery.Domain/Entities/ApplicationUser.cs
+++ OrderDelivery/OrderDelivery.Domain/Entities/ApplicationUser.cs
@@ -1,10 +1,10 @@
-﻿using OrderDelivery.Domain.Enums;
+﻿using Microsoft.AspNetCore.Identity;
+using OrderDelivery.Domain.Enums;
 
 namespace OrderDelivery.Domain.Entities
 {
-    public class ApplicationUser
+    public class ApplicationUser : IdentityUser<Guid>
     {
-        public Guid Id { get; set; }
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
         public string PersonalPhotoUrl { get; set; } = string.Empty;
@@ -12,8 +12,6 @@ namespace OrderDelivery.Domain.Entities
         public string NationalIdBackPhotoUrl { get; set; } = string.Empty;
         public string NationalIdNumber { get; set; } = string.Empty;
         public UserType UserType { get; set; }
-        public string PhoneNumber { get; set; } = string.Empty;
-        public bool LockoutEnabled { get; set; }
 
 
         // Navigation Properties (One-to-One)
